cc7879c832820bfe647894f652f077f454ea832a
Erick Huanca <erick@huanca.net>
2019-11-28 01:32:24 -0400

    adicoin de layout

---
+++ app/src/main/java/com/example/proyalmacenconservices/ProductReceiptActivity.java
@@ -0,0 +1,13 @@
+package com.example.proyalmacenconservices;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProductReceiptActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_receipt);
+    }
+}
